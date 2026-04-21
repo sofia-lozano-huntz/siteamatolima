1a153eb11825ab5c46ca7d4cd4e4b5332f2cef55
Sofia Lozano <sofia.lozano.huntz@gmail.com>
2026-04-21 14:53:40 -0300

    Create data.ts

---
+++ lib/data.ts
@@ -0,0 +1,30 @@
+export const villas = [
+  {
+    title: "Villa Aurora",
+    meta: "6 suítes • Trancoso",
+    description:
+      "Arquitetura aberta, materialidade natural e uma experiência privada pensada para estadias memoráveis.",
+    image: "/images/villa-1.jpg",
+  },
+  {
+    title: "Villa Brisa",
+    meta: "8 suítes • Angra",
+    description:
+      "Volumes silenciosos, integração com a paisagem e interiores sofisticados com atmosfera serena.",
+    image: "/images/villa-2.jpg",
+  },
+  {
+    title: "Villa Maré",
+    meta: "5 suítes • Bahia",
+    description:
+      "Um refúgio contemporâneo onde luz, sombra e textura constroem uma sensação de exclusividade.",
+    image: "/images/villa-3.jpg",
+  },
+];
+
+export const experiences = [
+  "Gastronomia privada",
+  "Wellness e spa",
+  "Experiências sob medida",
+  "Serviço concierge",
+];
