dc26c1ea4d016fa180f60586b6b353dfd2ea386e
Sofia Lozano <sofia.lozano.huntz@gmail.com>
2026-04-20 21:31:22 -0300

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -1,1 +1,17 @@
-
+export const portfolioItems = [
+  {
+    id: 1,
+    name: "Casa Tamboré 11",
+    image: "/images/portfolio/tambore.jpg",
+  },
+  {
+    id: 2,
+    name: "Apartamento Higienópolis",
+    image: "/images/portfolio/higienopolis.jpg",
+  },
+  {
+    id: 3,
+    name: "Residência Jardins",
+    image: "/images/portfolio/jardins.jpg",
+  },
+];
