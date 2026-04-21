e17acc1fe8468060b9e0f9c8e7712a316e1d8fa8
Sofia Lozano <sofia.lozano.huntz@gmail.com>
2026-04-20 21:30:42 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,19 @@
+import "./globals.css";
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Amato Lima",
+  description: "Ativos imobiliários | Real estate assets",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
